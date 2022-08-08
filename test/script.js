668f973dc2f2289a87fe7c8ce0ece3685e3fe671
Christian Tsalidis <ctsalidis1999@gmail.com>
2022-08-08 19:04:46 +0200

    Added slide in animation to the main container

--- test/script.js
+++ test/script.js
@@ -1,25 +1,44 @@
 // on appear animation
-var onAppear = [];
+var onAppearProjects = [];
+var onAppearMain = [];
 
 // ref --> https://codepen.io/zvona/pen/RwyyNK
 
 document.addEventListener("DOMContentLoaded", function() {
-onAppear = [].map.call(document.querySelectorAll(".onAppear"), function(item ) {
-    return item;
-});
+    onAppearProjects = [].map.call(document.querySelectorAll(".onAppear"), function(item ) {
+        return item;
+    });
+
+    onAppearMain = [].map.call(document.querySelectorAll(".onAppearMain"), function(item ) {
+        return item;
+    });
 }, false);
 
 window.addEventListener("scroll", function() {
-onAppear.forEach(function(elem) {
+    checkScroll(onAppearProjects, "visible-projects");
+    checkScroll(onAppearMain, "visible-main");
+}, false);
+
+function checkScroll(e, v) {
+    e.forEach(function(elem) {
+        if(calculateScrollToAppear(elem)) {
+            elem.classList.add(v);
+        } else {
+            elem.classList.remove(v);
+        }
+    });
+}
+
+function calculateScrollToAppear(elem) {
     var vwTop = window.pageYOffset;
     var vwBottom = (window.pageYOffset + window.innerHeight);
     var elemTop = elem.offsetTop;
     var elemHeight = elem.offsetHeight;
-    
-    if (vwBottom > elemTop && ((vwTop - elemHeight) < elemTop)) {
-    elem.classList.add("visible");
-    } else {
-    elem.classList.remove("visible");
+
+    if (vwBottom > elemTop && ((vwTop - elemHeight) < elemTop)) { 
+        return true;
     }
-});
-}, false);
+    else {
+        return false;
+    }
+}
