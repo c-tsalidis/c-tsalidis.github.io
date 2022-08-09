1028e256041d5dca7b0744df2bfd9b1799de20d3
Christian Tsalidis <ctsalidis1999@gmail.com>
2022-08-09 16:09:30 +0200

    removed some old stuff

--- Old/menuOverlay.js
+++
@@ -1,9 +0,0 @@
-function openNav()
-{
-  document.getElementById("menuOverlayNav").style.height = "100%";
-}
-
-function closeNav()
-{
-  document.getElementById("menuOverlayNav").style.height = "0%";
-}
